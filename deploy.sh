61a55122095af892538b6c1eece7dad58369e4d8
Hrushikesh Chikkerur <hchikkerur@gmail.com>
2025-06-27 12:29:23 +0530

    appspec file modified

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+# Set up kubeconfig for EKS
+aws eks update-kubeconfig --region us-wes-2 --name demo-mde
 
 echo "Running K8 deployment/....."
-kubectl apply -f k8s/ -n local
+kubectl apply -f k8s/ -n appln-test
--- validate.sh
+++ validate.sh
@@ -1,1 +1,7 @@
+set -e
+
+# Set up kubeconfig for EKS
+aws eks update-kubeconfig --region us-wes-2 --name demo-mde
+
+echo "validating pods status"
 kubectl get pods,all -n local
